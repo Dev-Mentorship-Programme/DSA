52ab3065727d8b58df84aef263862257c37b65e2
gbemi-cocoon <gbemisola-ext@cocoon.com>
2025-09-10 07:26:11 +0100

    fix(review): add comment to indicate that a -1 may be returned in some cases

--- arrays/solutions/javascript/largest-element.js
+++ arrays/solutions/javascript/largest-element.js
@@ -1,6 +1,7 @@
 /* author: @Barkty
 
 Given an array of positive integers arr[] of size n, the task is to find second largest distinct element in the array.
+Note: If the second largest element does not exist, return -1.
 */
 
 class SecondLargestElement {
